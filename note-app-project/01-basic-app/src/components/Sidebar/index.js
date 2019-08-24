7b9e7dcc463e5f1b94928667f774f33c8d3365c1
Swashata Ghosh <swashata@wpquark.com>
2019-08-24 17:29:08 +0530

    feat: auto-close sidebar on mobile

--- note-app-project/01-basic-app/src/components/Sidebar/index.js
+++ note-app-project/01-basic-app/src/components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -7,11 +7,41 @@ import penUrl from './pen.svg';
 import './style.scss';
 
 export default function Sidebar({ links }) {
+  // a state for toggling the menu
   const [visible, setVisible] = useState(false);
+  const asideRef = useRef();
+  const buttonRef = useRef();
+
+  // we want to close the open sidebar if clicked outside
+  // the sidebar
+  useEffect(() => {
+    const handler = e => {
+      // return if came from sidebar
+      if (asideRef.current && e.target && asideRef.current.contains(e.target)) {
+        return;
+      }
+      // return if came from the button, because it has it's own listener
+      if (
+        buttonRef.current &&
+        e.target &&
+        buttonRef.current.contains(e.target)
+      ) {
+        return;
+      }
+      // if it is already false, then react will not re-render it
+      // so we are good.
+      setVisible(false);
+    };
+    document.addEventListener('click', handler);
+
+    // return cleanup function
+    return () => document.removeEventListener('click', handler);
+  }, []);
 
   return (
     <>
       <aside
+        ref={asideRef}
         className={classNames('menu main-menu', {
           'menu-is-visible': visible,
         })}
@@ -32,6 +62,7 @@ export default function Sidebar({ links }) {
         )}
       </aside>
       <button
+        ref={buttonRef}
         className="menu-toggler button"
         onClick={e => {
           e.preventDefault();
