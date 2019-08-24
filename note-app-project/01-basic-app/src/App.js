5f20b5c2a74d35ef2115aa2eeff34beb0584282c
Swashata Ghosh <swashata@wpquark.com>
2019-08-24 17:11:21 +0530

    feat: add view note

--- note-app-project/01-basic-app/src/App.js
+++ note-app-project/01-basic-app/src/App.js
@@ -1,104 +1,17 @@
 import React, { useReducer, useEffect } from 'react';
-import uuid4 from 'uuid/v4';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './style.scss';
+
 import Sidebar from './components/Sidebar';
 import Nav from './components/Nav';
-
 import Home from './routes/Home';
-
-const initialNotes = [
-  {
-    id: uuid4(),
-    title: 'Sample Note',
-    note: `## Est relatu rigore facitis montibus bibulis
-
-Primos committere placet ille unda scires vicem pariturae ex rapiare quater
-violabere fulva patruelibus, carmen florentis culpa. Maenaliosque quoque sulcis
-curalium in palude flumineis texere, in inicit frondes. Tollite equam, infuso in
-erat iugulo turba. Dat adsere non, in cum longeque ergo.
-
-1. Est fatebor unica
-2. Non est umerique vise una
-3. Non nomen in quem umbra
-4. Pavonibus capiebat tormentis pugnare et faciem
-5. Portis Tarentum viro
-6. Respicit puerpera quicquid divesque defendere in foret`,
-  },
-  {
-    id: uuid4(),
-    title: 'Another Note',
-    note: `## Vidisti tamen
-
-    Inpositum habet deam, aeacides marisque aures, suco missus, tum qui seque. Unum
-    cui quas possis est orant inpulerat ensem, tactusque vibrata tormento linguas.
-    Digna exanimi suis perdere, tandem constantia abest tulitquemuneris canentis;
-    Gorgone pedes amens terrent. Nec iungere **voce**: dare **per** lanianda
-    progeniem vocis Oresteae incumbensque grata et Dolona Saturnia bimembres fuit
-    est viro. Postera tumulus caloris silices Damasicthona semel et mergit valetque
-    litora Nasamoniaci moras latebris, terga dolores amores castique.`,
-  },
-];
-
-const getPosition = (haystack, id) => {
-  const position = haystack.findIndex(item => {
-    return item.id === id;
-  });
-  if (position === -1) {
-    throw new Error('Invalid id passed to payload during notes update.');
-  }
-  return position;
-};
-
-function getInitialNotes() {
-  const fromLocalStorage = window.localStorage.getItem('note-app-notes');
-  if (fromLocalStorage) {
-    try {
-      return JSON.parse(fromLocalStorage);
-    } catch {
-      return initialNotes;
-    }
-  }
-  return initialNotes;
-}
-
-function setNotesToLocalStorage(notes) {
-  window.localStorage.setItem('note-app-notes', JSON.stringify(notes));
-}
-
-/**
- * Reducer for managing notes.
- *
- * @param {Array} state Current notes.
- * @param {Object} action Dispatcher action.
- */
-function notesReducer(state, action) {
-  if (action.type === 'add') {
-    return [action.payload, ...state];
-  } else if (action.type === 'update') {
-    const position = getPosition(state, action.payload.id);
-    return [
-      ...state.slice(0, position),
-      {
-        id: action.payload.id,
-        title: action.payload.title,
-        note: action.payload.note,
-      },
-      ...state.slice(position + 1),
-    ];
-  } else if (action.type === 'delete') {
-    const position = getPosition(state, action.payload.id);
-    return [...state.slice(0, position), ...state.slice(position + 1)];
-  }
-  throw new Error('Invalid type passed to action.');
-}
+import View from './routes/View';
+import Error404 from './components/404';
+import { useNotes, notesCtx } from './utils/note';
 
 function App() {
-  const [notes, dispatch] = useReducer(notesReducer, null, getInitialNotes);
-  useEffect(() => {
-    setNotesToLocalStorage(notes);
-  }, [notes]);
+  const [notes] = useNotes();
 
   const sidebarLinks = notes.map(note => ({
     label: note.title,
@@ -107,7 +20,7 @@ function App() {
 
   return (
     <Router>
-      <>
+      <notesCtx.Provider value={notes}>
         <header>
           <Nav />
         </header>
@@ -117,11 +30,13 @@ function App() {
             <div className="container">
               <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/note/:noteId" component={View} />
+                <Route component={Error404} />
               </Switch>
             </div>
           </div>
         </main>
-      </>
+      </notesCtx.Provider>
     </Router>
   );
 }
--- note-app-project/01-basic-app/src/routes/Home/index.js
+++ note-app-project/01-basic-app/src/routes/Home/index.js
@@ -9,15 +9,19 @@ export default function Home() {
       <div className="notes-home__imgwrap">
         <img src={logoUrl} className="notes-home__img" alt="Take Notes" />
       </div>
-      <h2 className="notes-home__title title is-2">Notes App</h2>
-      <h3 className="notes-home__subtitle subtitle is-3">
+      <h2 className="notes-home__title title is-2 has-text-black-ter">
+        Notes App
+      </h2>
+      <h3 className="notes-home__subtitle subtitle is-3 has-text-grey">
         Take notes, never forget
       </h3>
-      <p className="is-size-5">
+      <p className="is-size-5 has-text-grey-light">
         Welcome to your notes. Add new notes, read and edit existing and more.
         Synced live to all your devices.
       </p>
-      <p className="is-size-5">Click on NEW NOTES to get started.</p>
+      <p className="is-size-5 has-text-grey-light">
+        Click on NEW NOTES to get started.
+      </p>
     </section>
   );
 }
