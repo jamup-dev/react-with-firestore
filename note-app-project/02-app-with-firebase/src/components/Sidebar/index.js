0f015d8155090db69017e483c6275334dbff1506
Swashata Ghosh <swashata@wpquark.com>
2019-11-19 12:38:52 +0530

    chore: remove proptype
    We dont need it anymore.

--- note-app-project/02-app-with-firebase/src/components/Sidebar/index.js
+++ note-app-project/02-app-with-firebase/src/components/Sidebar/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import fuzzySearch from 'fuzzysearch';
 
