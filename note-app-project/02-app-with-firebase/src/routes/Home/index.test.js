a7d8c3f0b69276c7ea07ee6af2179dddd30274da
Swashata Ghosh <swashata@wpquark.com>
2019-11-22 17:59:03 +0530

    test: add more tests
    Specifically test the home route component.

--- note-app-project/02-app-with-firebase/src/routes/Home/index.test.js
+++ note-app-project/02-app-with-firebase/src/routes/Home/index.test.js
@@ -6,4 +6,30 @@ import {
 } from '../../utils/testHelpers';
 import Home from '.';
 
-describe('Home Route', () => {});
+describe('Home Route', () => {
+  test('shows sign in button if user not logged in', async () => {
+    const [app, auth] = getFirebaseAppAndAuth(null);
+    const { findByText } = render(
+      <TestAppProvider auth={auth} app={app}>
+        <Home />
+      </TestAppProvider>
+    );
+
+    const button = await findByText('Sign in');
+    expect(button).toBeInTheDocument();
+    expect(button.getAttribute('href')).toBe('/signin');
+  });
+
+  test('shows new note button if user is logged in', async () => {
+    const [app, auth] = getFirebaseAppAndAuth();
+    const { findByText } = render(
+      <TestAppProvider auth={auth} app={app}>
+        <Home />
+      </TestAppProvider>
+    );
+
+    const button = await findByText('New Note');
+    expect(button).toBeInTheDocument();
+    expect(button.getAttribute('href')).toBe('/new');
+  });
+});
